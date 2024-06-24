5307b0251588e2d1f06c8453043dac494dc7887e
oussama.trabelsi <oussama.trabelsidev@gmail.com>
2024-06-24 17:00:16 +0200

    feat(framework): updated dependency injections

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -9,11 +9,16 @@ import 'package:weather_app/features/city/domain/usecases/get_cities.dart';
 import 'package:weather_app/features/city/domain/usecases/get_selected_city.dart';
 import 'package:weather_app/features/city/domain/usecases/set_selected_city.dart';
 import 'package:weather_app/features/city/presentation/bloc/city_bloc.dart';
+import 'package:weather_app/features/weather/data/datasources/weather_info_remote_datasource.dart';
+import 'package:weather_app/features/weather/data/repositories/weather_info_repository_impl.dart';
+import 'package:weather_app/features/weather/domain/repositories/weather_info_repository.dart';
+import 'package:weather_app/features/weather/domain/usecases/get_weather_info.dart';
+import 'package:weather_app/features/weather/presentation/bloc/weather_info_bloc.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  /// Features - City
+  /** Features - City **/
   // Bloc
   sl.registerFactory(() => CityBloc(
         getCities: sl(),
@@ -38,6 +43,25 @@ Future<void> init() async {
   // Repository
   sl.registerLazySingleton<CityRepository>(
     () => CityRepositoryImpl(localDataSource: sl()),
+  );
+
+  /** Features - Weather **/
+  // Bloc
+  sl.registerFactory(() => WeatherInfoBloc(getWeatherInfo: sl()));
+
+  // Use cases
+  sl.registerLazySingleton(() => GetWeatherInfo(sl()));
+
+  // Repository
+  sl.registerLazySingleton<WeatherInfoRepository>(
+    () => WeatherInfoRepositoryImpl(
+      remoteDataSource: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<WeatherInfoRemoteDataSource>(
+    () => WeatherInfoRemoteDataSourceImpl(client: sl()),
   );
 
   ///  External
