6daf1c389917191bb1ef6eab48dabde965ff55a3
oussama.trabelsi <oussama.trabelsidev@gmail.com>
2024-06-24 12:31:12 +0200

    feat(framework): added a city repository

---
+++ lib/features/city/domain/repositories/city_repository.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:weather_app/core/error/failures.dart';
+import 'package:weather_app/features/city/domain/entities/city.dart';
+
+/// Abstract repository interface for managing city data.
+///
+/// This interface defines methods to interact with city data, including
+/// fetching cities, adding, deleting and selecting a city.
+abstract class CityRepository {
+  /// Retrieves a list of cities.
+  ///
+  /// Returns a [Future] that completes with an [Either] containing either
+  /// a [Failure] or a list of [City] instances.
+  Future<Either<Failure, List<City>>> getCities();
+
+  /// Adds a new city.
+  ///
+  /// [city] is the name of the city to be added.
+  /// Returns a [Future] that completes when the operation finishes.
+  Future<void> addCity(City city);
+
+  /// Deletes a city.
+  ///
+  /// [city] is the name of city to be deleted.
+  /// Returns a [Future] that completes when the operation finishes.
+  Future<void> deleteCity(City city);
+
+  /// Retrieves the currently selected city, if any.
+  ///
+  /// Returns a [Future] that completes with an [Either] containing either
+  /// a [Failure] or the currently selected [City].
+  Future<Either<Failure, City?>> getSelectedCity();
+
+  /// Sets the currently selected city.
+  ///
+  /// [city] is the name of the city to be set as the currently selected city.
+  /// Returns a [Future] that completes when the operation finishes.
+  Future<void> setSelectedCity(City city);
+}
