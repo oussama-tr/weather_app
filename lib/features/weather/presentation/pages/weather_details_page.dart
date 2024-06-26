b01f385ec7681314579069bcfc70dd77c5e58e1a
oussama.trabelsi <oussama.trabelsidev@gmail.com>
2024-06-26 02:22:23 +0200

    feat(application): weather details page

--- lib/features/weather/presentation/pages/weather_details_page.dart
+++ lib/features/weather/presentation/pages/weather_details_page.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/core/layout/weather_app_layout.dart';
+import 'package:weather_app/core/theme/weather_app_icons.dart';
 import 'package:weather_app/features/city/domain/entities/city.dart';
 import 'package:weather_app/features/city/presentation/bloc/city_bloc.dart';
 import 'package:weather_app/features/city/presentation/bloc/city_state.dart';
 import 'package:weather_app/features/weather/presentation/bloc/weather_info_bloc.dart';
+import 'package:weather_app/features/weather/presentation/widgets/day_cycle_widget.dart';
+import 'package:weather_app/features/weather/presentation/widgets/icon_card_widget.dart';
+import 'package:weather_app/features/weather/presentation/widgets/overview_widget.dart';
+import 'package:weather_app/features/weather/presentation/widgets/temperature_widget.dart';
 
 class WeatherDetailsPage extends StatelessWidget {
   const WeatherDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather Details'),
-      ),
-      body: SafeArea(
+    return WeatherAppLayout(
+      appBarTitle: 'Weather details',
+      child: SafeArea(
         child: BlocBuilder<CityBloc, CityState>(
           builder: (context, cityState) {
             if (cityState is CitiesLoaded && cityState.selectedCity != null) {
@@ -33,9 +37,66 @@ class WeatherDetailsPage extends StatelessWidget {
                     child: CircularProgressIndicator(),
                   );
                 } else if (weatherState is WeatherInfoLoaded) {
-                  return Center(
-                    child: Text(
-                      weatherState.weatherInfo.toString(),
+                  final weatherInfo = weatherState.weatherInfo;
+
+                  return SingleChildScrollView(
+                    physics: const ClampingScrollPhysics(),
+                    child: Column(
+                      children: [
+                        OverviewWidget(
+                          cityName: weatherInfo.cityName,
+                          country: weatherInfo.country,
+                          mainWeather: weatherInfo.main,
+                          weatherDescription: weatherInfo.description,
+                          feelsLike: weatherInfo.feelsLike,
+                          cloudsCoverage: weatherInfo.cloudsAll,
+                          iconCode: weatherInfo.iconCode,
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: IconCardWidget(
+                                icon: WeatherAppIcons.wind,
+                                title: 'Wind',
+                                data:
+                                    'speed ${weatherInfo.windSpeed} km/h ${weatherInfo.windDeg}°',
+                              ),
+                            ),
+                            Expanded(
+                              child: IconCardWidget(
+                                icon: WeatherAppIcons.barometer,
+                                title: 'Pressure',
+                                data: '${weatherInfo.pressure} hPa',
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        TemperatureWidget(
+                          temp: weatherInfo.temp,
+                          tempMin: weatherInfo.tempMin,
+                          tempMax: weatherInfo.tempMax,
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: IconCardWidget(
+                                icon: WeatherAppIcons.humidity,
+                                title: 'Humidity',
+                                data: '${weatherInfo.humidity} g/kg',
+                              ),
+                            ),
+                            Expanded(
+                              child: DayCycleWidget(
+                                sunrise: weatherInfo.sunrise,
+                                sunset: weatherInfo.sunset,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   );
                 } else if (weatherState is WeatherInfoError) {
